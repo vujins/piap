8fa8edddb7881c595683bd1bb547745741a16705
Strahinja Vujin <strahinja.vujin11@gmail.com>
2018-09-04 14:43:58 +0200

    init commit

---
+++ src/main/java/Library.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.SpringApplication;
+
+public class Library {
+	
+	 public static void main(String[] args) {
+	        SpringApplication.run(Library.class, args);
+	    }
+}
